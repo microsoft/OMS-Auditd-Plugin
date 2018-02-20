2beac19fea0d321e6a7bc9b0d362a232a6887d36
jobooth <jobooth@microsoft.com>
2018-02-20 12:39:39 +0000

    Added additional call to auparse_first_field() to ensure all fields are included in creating records.

--- AuditEventProcessor.cpp
+++ AuditEventProcessor.cpp
@@ -133,18 +133,14 @@ bool AuditEventProcessor::process_execve()
 {
     static const std::unordered_set<std::string> execve_fields = { "arch", "syscall", "success", "exit", "items", "ppid", "pid", "auid", "uid", "gid", "euid", "suid", "fsuid", "egid", "sgid", "fsgid", "tty", "ses", "comm", "exe", "key", "name", "inode", "dev", "mode", "ouid", "ogid", "rdev", "nametype", "cwd", "cmdline" };
 
-    if (_num_records < 5 || auparse_get_type(_state) != AUDIT_SYSCALL) {
+    if (auparse_first_record(_state) != 1 || _num_records < 5 || auparse_get_type(_state) != AUDIT_SYSCALL) {
         return false;
     }
 
     auparse_find_field(_state, "syscall");
     const char *syscall = auparse_interpret_field(_state);
 
-    if (auparse_first_field(_state) != 1) {
-        return false;
-    }
-
-    if (syscall == nullptr || strncmp(syscall, "execve", 6) != 0) {
+    if (auparse_first_field(_state) != 1 || syscall == nullptr || strncmp(syscall, "execve", 6) != 0) {
         return false;
     }
 
@@ -233,14 +229,14 @@ bool AuditEventProcessor::process_execve()
             case AUDIT_CWD: {
                 const char* field = auparse_find_field(_state, "cwd");
                 if (field == nullptr || !process_field("cwd")) {
-                    break;
+                    continue;
                 };
                 break;
             }
             case AUDIT_PATH: {
                 const char* field = auparse_find_field(_state, "item");
                 if (field == nullptr || strcmp(field, "0") != 0) {
-                    break;
+                    continue;
                 }
 
                 auparse_first_field(_state);
@@ -304,16 +300,14 @@ void AuditEventProcessor::callback(void *ptr)
         return;
     }
 
-    if (auparse_first_record(_state) != 1) {
-        Logger::Warn("auparse_first_record() failed!");
-        return;
-    }
-
     if (process_execve()) {
         return;
     }
 
-    auparse_first_record(_state);
+    if (auparse_first_record(_state) != 1) {
+        Logger::Warn("auparse_first_record() failed!");
+        return;
+    }
 
     if (!begin_event()) {
         return;
@@ -353,6 +347,11 @@ void AuditEventProcessor::callback(void *ptr)
                 throw std::runtime_error("Queue closed");
             }
             cancel_event();
+            return;
+        }
+
+        if (auparse_first_field(_state) != 1) {
+            Logger::Warn("auparse_first_field() failed!");
             return;
         }
 
