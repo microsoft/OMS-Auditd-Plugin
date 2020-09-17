6737b8de5722283a03a21a773096ff9d4797f3a5
Tad Glines <taglines@microsoft.com>
2020-09-17 08:10:55 -0700

    Fix CDPx Build

--- build/cdpx_sign.cmd
+++ build/cdpx_sign.cmd
@@ -13,7 +13,17 @@ dir %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\BuildPackages
 dir %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\BuildPackages\outputs
 dir %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\BuildPackages\outputs\build
 
+if NOT EXIST %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\BuildPackages\outputs\build exit /b 0
+
 Xcopy \Y %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\BuildPackages\outputs\build\*.deb signed
 Xcopy \Y %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\BuildPackages\outputs\build\*.rpm signed
 
-dir signed
+dir signed\*.deb
+
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+dir signed\*.rpm
+
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+exit /b 0
