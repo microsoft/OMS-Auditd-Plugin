3757c430bbe1189898ef400aa5af485455dc442e
Tad Glines <taglines@microsoft.com>
2020-09-16 20:20:03 -0700

    Fix CDPx Build

--- build/cdpx_sign.cmd
+++ build/cdpx_sign.cmd
@@ -5,5 +5,5 @@ cd ..
 
 if EXIST signed rmdir /s signed
 mkdir signed
-copy %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%/current/drop/BuildPackages/outputs/build/*.deb signed
-copy %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%/current/drop/BuildPackages/outputs/build/*.rpm signed
+copy %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\BuildPackages\outputs\build\*.deb signed
+copy %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\BuildPackages\outputs\build\*.rpm signed
