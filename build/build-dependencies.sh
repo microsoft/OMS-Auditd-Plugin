2fa9b09647b070628b3c807073b2f4d0e1cdf861
Vijay <vnadella@microsoft.com>
2024-08-16 10:49:03 -0500

    fix for dir

--- build/build-dependencies.sh
+++ build/build-dependencies.sh
@@ -140,7 +140,7 @@ ninja install
 popd
 
 cp -r $tmpdirSystemd/install/include/* $IncludeDir/
-cp $tmpdirSystemd/install/lib/systemd $LibDir/
+cp -r $tmpdirSystemd/install/lib/systemd $LibDir/
 
 echo "Copy of systemd complete"
 # Copy headers and static library to the include and lib directories
