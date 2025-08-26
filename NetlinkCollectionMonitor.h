771776cb2dbe3be6c6a7315daebbec7bf03394d8
Sriram Nambakam <snambakam@microsoft.com>
2025-08-26 23:33:19 +0000

    Bring ICollectionMonitor.h CollectionMonitor.h NetlinkCollectionMonitor.h NetlinkCollectionMonitor.cpp from c28670127841d89cd0c3b1b8efabc318f0bc68b8 (partial cherry-pick)

--- NetlinkCollectionMonitor.h
+++ NetlinkCollectionMonitor.h
@@ -14,8 +14,8 @@
     THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#ifndef AUOMS_COLLECTIONMONITOR_H
-#define AUOMS_COLLECTIONMONITOR_H
+#ifndef NETLINK_COLLECTIONMONITOR_H
+#define NETLINK_COLLECTIONMONITOR_H
 
 #include "ICollectionMonitor.h"
 #include "Netlink.h"
@@ -25,25 +25,42 @@
 #include <chrono>
 #include <set>
 
-class CollectionMonitor: public ICollectionMonitor {
+class NetlinkCollectionMonitor : public ICollectionMonitor {
 public:
     static constexpr int COLLECTOR_RESTART_WINDOW = 30;
     static constexpr int MAX_COLLECTOR_RESTARTS = 15; // The maximum times the collector will be restarted within COLLECTOR_RESTART_WINDOW seconds before restarts are disabled.
 
-    CollectionMonitor(std::shared_ptr<PriorityQueue> queue,
-                      const std::string& auditd_path,
-                      const std::string& collector_path,
-                      const std::string& collector_config_path)
-            : _builder(std::make_shared<EventQueue>(std::move(queue)), nullptr),
-              _auditd_path(auditd_path), _collector_path(collector_path), _collector_config_path(collector_config_path),
-              _collector(collector_path, collector_args(collector_config_path), Cmd::PIPE_STDIN), _audit_pid(0), _pause_collector_check(false), _pause_time(), _last_audit_pid_report(), _collector_restarts() {}
-
+    NetlinkCollectionMonitor(
+        std::shared_ptr<PriorityQueue> queue,
+        const std::string& collector_path,
+        const std::string& collector_config_path)
+    :   _builder(std::make_shared<EventQueue>(std::move(queue)), nullptr),
+        _collector_path(collector_path),
+        _collector_config_path(collector_config_path),
+        _collector(
+            collector_path,
+            collector_args(collector_config_path),
+            Cmd::PIPE_STDIN
+        ),
+        _pause_collector_check(false),
+        _pause_time(),
+        _last_audit_pid_report(),
+        _collector_restarts() {
+        }
 protected:
     void run() override;
     void on_stop() override;
-
 private:
-    std::vector<std::string> collector_args(const std::string& collector_config_path) {
+    // Return true if child is alive, false if not. If wait is true and child is alive, will wait forever for child to exit.
+    bool check_child(bool wait);
+    void start_collector();
+    void signal_collector(int signal);
+    bool is_collector_alive();
+    void report_proc_exit_status(Cmd& cmd);
+
+    std::vector<std::string> collector_args(
+        const std::string& collector_config_path
+    ) {
         std::vector<std::string> args;
         args.emplace_back("-n");
         if (!collector_config_path.empty()) {
@@ -53,30 +70,15 @@ private:
         return args;
     }
 
-
-    // Return true if child is alive, false if not. If wait is true and child is alive, will wait forever for child to exit.
-    bool check_child(bool wait);
-    void start_collector();
-    void signal_collector(int signal);
-    bool is_auditd_present();
-    bool is_collector_alive();
-    void send_audit_pid_report(int pid);
-    bool is_auditd_enabled_systemd();
-    bool is_auditd_enabled_sysv();
-    bool is_auditd_enabled_upstart();
-
     Netlink _netlink;
     EventBuilder _builder;
-    std::string _auditd_path;
     std::string _collector_path;
     std::string _collector_config_path;
     Cmd _collector;
-    uint32_t _audit_pid;
     bool _pause_collector_check;
     std::chrono::steady_clock::time_point _pause_time;
     std::chrono::steady_clock::time_point _last_audit_pid_report;
     std::set<std::chrono::steady_clock::time_point> _collector_restarts;
 };
 
-
-#endif //AUOMS_COLLECTIONMONITOR_H
+#endif /* NETLINK_COLLECTIONMONITOR_H */
